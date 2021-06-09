82132c7336dea338635bf2393807e82bdfb1c8bb
Sanjay-RD <rishidevsanjay9@gmail.com>
2021-06-09 18:17:25 +0545

    Prepare For Deployment

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,6 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -45,6 +41,18 @@ app.get("/api/config/paypal", (req, res) => {
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("hello");
+  });
+}
 
 app.use(notFound);
 
